8e232601cbc0a8385719e6a3dae29964aab8bb59
songzihengs.666 <songziheng.666@bytedance.com>
2023-09-15 13:49:57 +0800

    f

--- tax_calc.py
+++ tax_calc.py
@@ -1,4 +1,4 @@
-def calcPayableValue(monthlySalary, mounthCnt=12):
+def calcPayableValue(monthlySalary, mounthCnt=12, nianzhongjiang_month=0):
     """
     计算应纳税所得额
     1. 减去 每月5k 免税额度
@@ -6,20 +6,34 @@ def calcPayableValue(monthlySalary, mounthCnt=12):
     3. 减去专项扣除
     """
     total = monthlySalary * mounthCnt
-    #socialInsuranceMax = 28221
-    SOCIAL_INSURANCE_MAX = 28221 # 北京最高社保基数
-    Basic_retirement_insurance_frac = 0.08 # 退休个人部分
-    Basic_medical_insurance_frac = 0.02 # 医疗保险个人部分
-    Unemployment_insurance_frac = 0.002 # 失业金个人
-    Provident_Fund_frac = 0.12 # 公积金
-    monthly_basic_social_insurance_payable = min(SOCIAL_INSURANCE_MAX, monthlySalary) # 真实社保基数
-    social_insurance_value = 12 * monthly_basic_social_insurance_payable * \
-      (Basic_retirement_insurance_frac + Basic_medical_insurance_frac + Provident_Fund_frac + Unemployment_insurance_frac)
-    Special_deduction = 1000 * 12 # 专项扣除 房贷
-    Tax_exemption = 5000 * 12 # 基础免税额度
 
-    payable = total - Tax_exemption - Special_deduction - social_insurance_value
-    return payable
+    # 社保扣除
+    shebao_base = 33387 # 北京最高社保基数
+    yanglao_rate = 0.08 # 退休个人部分
+    yibao_rate = 0.02 # 医疗保险个人部分
+    shiye_rate = 0.002 # 失业金个人
+    gongjijin_rate = 0.12 # 公积金
+    shebao_base_real = min(shebao_base, monthlySalary) # 真实社保基数
+    shebao_kouchu_value = 12 * shebao_base_real * \
+      (yanglao_rate + yibao_rate + shiye_rate + gongjijin_rate)
+
+    # 公积金收入
+    gongjijin_income = shebao_base_real * gongjijin_rate * 2 * 12
+    
+    # 专项扣除
+    special_kouchu_value = 1000 * 12 # 专项扣除 房贷
+    tax_kouchu_value = 5000 * 12 # 基础免税额度
+
+    # 计税金额
+    tax_calc_val = total - tax_kouchu_value - special_kouchu_value - shebao_kouchu_value
+    tax_value = calcTax(tax_calc_val)
+
+    nianzhongjian_total = nianzhongjiang_month * monthlySalary
+    nianzhongjiang_income = nianzhongjian_total - calcNianzhongTax(nianzhongjian_total)
+
+    non_tax_income = total - tax_value + gongjijin_income + nianzhongjiang_income
+
+    return tax_calc_val, non_tax_income, gongjijin_income, nianzhongjian_total
 
 def calcTax(payable_val):
     """
@@ -37,9 +51,35 @@ def calcTax(payable_val):
     ]
     for i in range(len(rank) - 1):
         if rank[i + 1][0] > payable_val >= rank[i][0]:
+            print(rank[i][1] )
             return payable_val * rank[i][1] - rank[i][2]
     return payable_val * rank[7][1] - rank[7][2]
 
-payable_val = calcPayableValue(42000, 15)
-taxVal = calcTax(payable_val)
+
+def calcNianzhongTax(payable_val):
+
+    yuedu_jiangjin = payable_val / 12
+    # 低 高 税率 速算扣除
+    level = [
+        (0,     3000,  0.03, 0),
+        (3000,  12000, 0.1,  210),
+        (12000, 25000, 0.2,  1410),
+        (25000, 35000, 0.25, 2660),
+        (35000, 55000, 0.3,  44100),
+        (55000, 80000, 0.35, 7160),
+        (80000, 1e7,   0.45, 15160),
+    ]
+
+    for i, level in enumerate(level):
+        if level[0] <= yuedu_jiangjin < level[1]:
+            return (yuedu_jiangjin * level[2] - level[3]) * 12
+    return -1
+
+
+tax_calc_val, non_tax_income, gongjijin, nianzhongjian = calcPayableValue(52000, nianzhongjiang_month=3)
+print("公积金={}".format(gongjijin))
+taxVal = calcTax(tax_calc_val)
+print("纳税金={}".format(taxVal))
+print("年终奖={}".format(nianzhongjian))
+print("税后收入={}".format(non_tax_income - taxVal))
 
